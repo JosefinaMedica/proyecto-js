4ebaf8851789ceaefbcf05aba4b61a165143a6ff
Josefina Medica <medicajosefina@gmail.com>
2023-05-19 11:03:38 -0300

    cambios en la primer pre entrega

--- js/main.js
+++ js/main.js
@@ -1,34 +1,57 @@
 
+// alert("Calcular total de compra");
+
+
+// function calcularTotal (){
+
+
+// let precioProducto1 = parseInt(prompt("Ingrese el precio del Producto 1:"));
+// let precioProducto2 = parseInt(prompt("Ingrese el precio del Producto 2:"));
+// let precioProducto3 = parseInt(prompt("Ingrese el precio del Producto 3:"));
+
+// let Total = 0;
+
+// if (precioProducto1) {
+//     Total += precioProducto1;
+// }
+
+// if (precioProducto2) {
+//     Total += precioProducto2;
+// }
+
+// if (precioProducto3) {
+//     Total += precioProducto3;
+// }
+
+
+// alert("Tu total: $" + Total);
+// console.log("Tu total: $" + Total);
+
+// }
+
+// calcularTotal ();
+
+
 alert("Calcular total de compra");
 
+function calcularTotal() {
+  let total = 0;
+  const numProductos = 3;
 
-function calcularTotal (){
+  for (let i = 1; i <= numProductos; i++) {
+    let precioProducto = parseInt(prompt("Ingrese el precio de la Cruz " + i + ":"));
 
+    if (precioProducto) {
+      total += precioProducto;
+    } else {
+      i--; 
+    }
+  }
 
-let precioProducto1 = parseInt(prompt("Ingrese el precio del Producto 1:"));
-let precioProducto2 = parseInt(prompt("Ingrese el precio del Producto 2:"));
-let precioProducto3 = parseInt(prompt("Ingrese el precio del Producto 3:"));
-
-let Total = 0;
-
-if (precioProducto1) {
-    Total += precioProducto1;
+  alert("Tu total: $" + total);
+  console.log("Tu total: $" + total);
 }
 
-if (precioProducto2) {
-    Total += precioProducto2;
-}
-
-if (precioProducto3) {
-    Total += precioProducto3;
-}
-
-
-alert("Tu total: $" + Total);
-console.log("Tu total: $" + Total);
-
-}
-
-calcularTotal ();
+calcularTotal();
 
 
