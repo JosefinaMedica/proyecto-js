7681be77834682cae948ba6eaa7fcc20608a1adf
Josefina Medica <medicajosefina@gmail.com>
2023-06-02 14:24:11 -0300

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -32,26 +32,71 @@
 // calcularTotal ();
 
 
+// alert("Calcular total de compra");
+
+// function calcularTotal() {
+//   let total = 0;
+//   const numProductos = 3;
+
+//   for (let i = 1; i <= numProductos; i++) {
+//     let precioProducto = parseInt(prompt("Ingrese el precio de la Cruz " + i + ":"));
+
+//     if (precioProducto) {
+//       total += precioProducto;
+//     } else {
+//       i--; 
+//     }
+//   }
+
+//   alert("Tu total: $" + total);
+//   console.log("Tu total: $" + total);
+// }
+
+// calcularTotal();
+
+
+
 alert("Calcular total de compra");
 
 function calcularTotal() {
   let total = 0;
   const numProductos = 3;
+  let productos = [];
+  let precios = {};
 
   for (let i = 1; i <= numProductos; i++) {
     let precioProducto = parseInt(prompt("Ingrese el precio de la Cruz " + i + ":"));
 
     if (precioProducto) {
       total += precioProducto;
+      productos.push({
+        nombre: "Cruz " + i,
+        precio: precioProducto
+      });
+      precios["Cruz " + i] = precioProducto;
     } else {
-      i--; 
+      i--;
     }
   }
 
   alert("Tu total: $" + total);
   console.log("Tu total: $" + total);
+  console.log("Productos: ", productos);
+  console.log("Precios: ", precios);
+
+
+  let nombreProducto = prompt("Ingrese el nombre del producto a buscar:");
+  let productoEncontrado = productos.find(producto => producto.nombre === nombreProducto);
+
+  if (productoEncontrado) {
+    console.log("Producto encontrado:", productoEncontrado);
+  } else {
+    console.log("Producto no encontrado");
+  }
+
+  let precioFiltro = parseInt(prompt("Ingrese el precio máximo para filtrar los productos:"));
+  let productosFiltrados = productos.filter(producto => producto.precio <= precioFiltro);
+  console.log("Productos filtrados:", productosFiltrados);
 }
 
 calcularTotal();
-
-
